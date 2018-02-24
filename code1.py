fcf2a21228d4b1068a1dbd625a754fde6a8acf35
redashu <ashutoshh@linux.com>
2018-02-24 12:38:10 +0530

    msg3

--- code1.py
+++ code1.py
@@ -3,10 +3,10 @@
 from time import sleep
 
 def  check():
-	print("Hello world !!..")
+	print("Hello world welcome to jenkins !!..")
 	sleep(2)
 	print("Deal with CI/CD !!..")
-
+	sleep(1)
 
 
 if  __name__ == "__main__"  :
