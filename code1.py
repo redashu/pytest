cac9cdd86cb78c0aa3270c7d967d150b5bdc3d9c
redashu <ashutoshh@linux.com>
2018-02-24 12:26:45 +0530

    ddd

---
+++ code1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from time import sleep
+
+def  check():
+	while True:
+		print("Hello world !!..")
+		sleep(2)
+		print("Deal with CI/CD !!..")
+
+
+
+if  __name__ == "__main__"  :
+	check()
